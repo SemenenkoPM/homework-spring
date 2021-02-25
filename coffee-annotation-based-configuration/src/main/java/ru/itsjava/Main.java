dbf3b15817ed49b6439e3af714e7a3200eec1dd0
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2021-02-25 12:44:55 +0300

    added @ComponentScan({"ru.itsjava"})

--- coffee-annotation-based-configuration/src/main/java/ru/itsjava/Main.java
+++ coffee-annotation-based-configuration/src/main/java/ru/itsjava/Main.java
@@ -4,7 +4,7 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import ru.itsjava.service.CoffeeHouse;
-@ComponentScan
+@ComponentScan({"ru.itsjava"})
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
--- coffee-java-based-configuration/src/main/java/ru/itsjava/Main.java
+++ coffee-java-based-configuration/src/main/java/ru/itsjava/Main.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import ru.itsjava.service.CoffeeHouse;
 
-@ComponentScan
+@ComponentScan({"ru.itsjava"})
 public class Main {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
