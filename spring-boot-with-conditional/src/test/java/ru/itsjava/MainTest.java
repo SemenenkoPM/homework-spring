f5bdc903d46479d31dfa43b93d65395052db9c03
Pavel Semenenko <pavelsemenenko.spb@gmail.com>
2021-03-04 19:18:47 +0300

    added spring test, added dependency test

---
+++ spring-boot-with-conditional/src/test/java/ru/itsjava/MainTest.java
@@ -0,0 +1,10 @@
+package ru.itsjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MainTest {
+    @Test
+    public void contextLoad(){}
+}
